f12ad8d6e727096ea28016f5c82a0812c8833780
Rusakova Sofia <sonya.sesofa@gmail.com>
2019-07-03 03:01:31 +0300

    new buffer

--- buffer.py
+++ buffer.py
@@ -1,19 +1,12 @@
 class Buffer:
     def __init__(self):
         self.buf = list()
-        self.summa = 0
-        self.c = 0
-    
+
     def add(self, *a):
-        for i in a:
-            self.buf.append(i)
-            self.summa += i
-            self.c += 1
-            if self.c == 5:
-                print(self.summa)
-                self.summa = 0
-                self.c = 0
-                self.buf = self.buf[5:]
-        
+        self.buf += a
+        for i in range(len(self.buf)//5):
+            print(sum(self.buf[:5]))
+            self.buf=self.buf[5:]
+                
     def get_current_part(self):
         return self.buf
