0b452273efb5206d0aca35d849a8d15b1f16ab46
Rusakova Sofia <sonya.sesofa@gmail.com>
2019-07-06 01:50:43 +0300

    exception

---
+++ myException.py
@@ -0,0 +1,9 @@
+class NonPositiveError(ArithmeticError):
+    pass
+
+class PositiveList(list):
+    def append(self,x):
+        if x>0:
+            super(PositiveList, self).append(x)
+        else:
+            raise NonPositiveError()
